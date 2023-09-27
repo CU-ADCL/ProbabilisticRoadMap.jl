bfbd9bc607f8a1a1d78bc7eaa0766a3516b0dac8
himanshugupta1009 <himanshu.gupta@colorado.edu>
2023-09-27 15:57:00 -0600

    Add code for testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,34 @@
 using ProbabilisticRoadMap
 using Test
+using Graphs
+
+function check_prm(prm)
+
+    expected_node_values = [
+    PRMNodeValues(40.81692590363648, 51.595291733725055),
+    PRMNodeValues(86.03462665489386, 63.485622120651676),
+    PRMNodeValues(58.28199336036355, 69.55492562954907),
+    PRMNodeValues(96.1567156782461, 18.774735921078943),
+    PRMNodeValues(16.46442417471747, 56.30896022795546)
+    ]
+
+    expected_edges = [
+    Edge(1,3),
+    Edge(1,5),
+    Edge(2,3),
+    Edge(2,4),
+    Edge(4,5)
+    ]
+
+    received_node_values = [get_prop(prm,i,:values) for i in vertices(prm)]
+    received_edges = collect(edges(prm))
+
+    return ( (expected_edges==received_edges) && (expected_node_values==received_node_values))
+end
 
 @testset "ProbabilisticRoadMap.jl" begin
-    # Write your tests here.
+
+    include("../examples/square_env.jl")
+    @test check_prm(prm)
+
 end
