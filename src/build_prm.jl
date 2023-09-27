493fe08fe45fc1dd81264369f4559d21754b1d6c
himanshugupta1009 <himanshu.gupta@colorado.edu>
2023-09-27 12:27:25 -0600

    Add documentation

--- src/build_prm.jl
+++ src/build_prm.jl
@@ -1,3 +1,34 @@
+"""
+    add_prm_node!(prm, get_node_values, is_node_valid, rng, time_limit = 0.2)
+
+The function tries to add a new node to the given PRM within the specified time_limit.
+
+# Arguments
+
+- `prm` -> graph to which a new node (vertex) will be added
+- `get_node_values` -> function that returns the values to be stored at that node. It takes in a random number generator as an input and returns the node values \n
+    (For a 2D environment, the node values could be a tuple of x and y positions)
+```julia-repl
+        node_values = get_node_values(rng)
+```
+- `is_node_valid` -> function that checks if a given node is valid to be added to the PRM. It takes in the prm (graph) and node values as inputs and returns true or false \n
+    (This function can be used to check if the node values are in free space or not)
+```julia-repl
+        is_node_valid(prm,node_values)
+```
+- `rng` -> a random number generator object
+- `time_limit` (optional; default_value=0.2) -> specifies the time limit for adding a new node to the prm (graph)
+
+# Output
+
+- `true` if a new node has been successfully added to the prm (graph), else `false`
+
+# Example
+```julia-repl
+add_prm_node!(prm, get_node_values, is_node_valid, MersenneTwister(11),1.0)
+```
+
+"""
 function add_prm_node!(prm, get_node_values, is_node_valid, rng, time_limit = 0.2)
 
     node_values = get_node_values(rng)
@@ -16,8 +47,37 @@ function add_prm_node!(prm, get_node_values, is_node_valid, rng, time_limit = 0.
     if(!node_found)
         println("Node couldn't be added within the given time limit.")
     end
+    return node_found
 end
 
+
+"""
+    add_prm_edges!(prm,src_node_num,max_num_edges,is_edge_valid,edge_cost)
+
+The function adds edges for a given node in the prm (graph).
+
+# Arguments
+
+- `prm` -> graph to which new edges will be added
+- `src_node_num` -> the node number in the prm (graph) for which new edges will be added
+- `max_num_edges` -> the maximum number of edges this node can have in the prm (graph)
+- `is_edge_valid` -> function that checks if a given edge is valid to be added to the graph. It takes in the prm (graph), source node values and destination node values as inputs and returns true or false \n
+    (This function can be used to check if the edge connecting the node with source node values and the node with destination node values pass through an obstacle in the environment or not)
+```julia-repl
+        is_edge_valid(prm,src_node_values,des_node_values)
+```
+- `edge_cost` -> function that computes the cost of an edge between two nodes of the graph. It takes in the prm (graph), source node values and destination node values as inputs and returns the cost of the edge connecting these nodes \n
+    (For a 2D environment where the node values are (x,y) positions, this function can return the euclidean distance between the two nodes
+```julia-repl
+        edge_cost(prm,src_node_values,des_node_values)
+```
+
+# Example
+```julia-repl
+add_prm_edges!(prm, 11, 5, is_edge_valid, edge_cost)
+```
+
+"""
 function add_prm_edges!(prm,src_node_num,max_num_edges,is_edge_valid,edge_cost)
 
     #=
@@ -61,6 +121,47 @@ function add_prm_edges!(prm,src_node_num,max_num_edges,is_edge_valid,edge_cost)
     end
 end
 
+
+"""
+    generate_prm(num_nodes,max_edges,get_node_values,is_node_valid,is_edge_valid,edge_cost,rng,time_limit=0.2)
+
+The function adds edges for a given node in the prm (graph).
+
+# Arguments
+
+- `num_nodes` -> graph to which new edges will be added
+- `max_edges` -> the node number in the prm (graph) for which new edges will be added
+- `max_num_edges` -> the maximum number of edges this node can have in the prm (graph)
+- `get_node_values` -> function that returns the values to be stored at that node. It takes in a random number generator as an input and returns the node values \n
+    (For a 2D environment, the node values could be a tuple of x and y positions)
+```julia-repl
+        node_values = get_node_values(rng)
+```
+- `is_node_valid` -> function that checks if a given node is valid to be added to the PRM. It takes in the prm (graph) and node values as inputs and returns true or false \n
+    (This function can be used to check if the node values are in free space or not)
+```julia-repl
+        is_node_valid(prm,node_values)
+```
+- `is_edge_valid` -> function that checks if a given edge is valid to be added to the graph. It takes in the prm (graph), source node values and destination node values as inputs and returns true or false \n
+    (This function can be used to check if the edge connecting the node with source node values and the node with destination node values pass through an obstacle in the environment or not)
+```julia-repl
+        is_edge_valid(prm,src_node_values,des_node_values)
+```
+- `edge_cost` -> function that computes the cost of an edge between two nodes of the graph. It takes in the prm (graph), source node values and destination node values as inputs and returns the cost of the edge connecting these nodes \n
+    (For a 2D environment where the node values are (x,y) positions, this function can return the euclidean distance between the two nodes
+```julia-repl
+        edge_cost(prm,src_node_values,des_node_values)
+```
+- `rng` -> a random number generator object
+- `time_limit` (optional; default_value=0.2) -> specifies the time limit for adding a new node to the prm (graph)
+
+
+# Example
+```julia-repl
+generate_prm(100,5,get_node_values,is_node_valid,is_edge_valid,edge_cost,MersenneTwister(11),1.0)
+```
+
+"""
 function generate_prm(num_nodes,max_edges,get_node_values,is_node_valid,is_edge_valid,edge_cost,rng,time_limit=0.2)
 
     prm = MetaGraph()
