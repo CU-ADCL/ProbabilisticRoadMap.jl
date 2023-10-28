a914d97207b60974c3f072dea4a46cb9177d29b8
himanshugupta1009 <himanshu.gupta@colorado.edu>
2023-10-27 21:37:06 -0600

    Modify test file to avoid errors

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ using Graphs
 function check_prm(prm)
 
     expected_node_values = [
-    PRMNodeValues(40.81692590363648, 51.595291733725055),
-    PRMNodeValues(86.03462665489386, 63.485622120651676),
-    PRMNodeValues(58.28199336036355, 69.55492562954907),
-    PRMNodeValues(96.1567156782461, 18.774735921078943),
-    PRMNodeValues(16.46442417471747, 56.30896022795546)
+    PRMNodeState(40.81692590363648, 51.595291733725055),
+    PRMNodeState(86.03462665489386, 63.485622120651676),
+    PRMNodeState(58.28199336036355, 69.55492562954907),
+    PRMNodeState(96.1567156782461, 18.774735921078943),
+    PRMNodeState(16.46442417471747, 56.30896022795546)
     ]
 
     expected_edges = [
@@ -20,7 +20,7 @@ function check_prm(prm)
     Edge(4,5)
     ]
 
-    received_node_values = [get_prop(prm,i,:values) for i in vertices(prm)]
+    received_node_values = [get_prop(prm,i,:state) for i in vertices(prm)]
     received_edges = collect(edges(prm))
 
     return ( (expected_edges==received_edges) && (expected_node_values==received_node_values))
